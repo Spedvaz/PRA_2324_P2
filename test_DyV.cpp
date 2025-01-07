e5cc1ee5e3f78348655e8a58026532c406013512
Sergio Pedraza Vázquez <spedvaz@etsinf.upv.es>
2025-01-07 11:06:39 +0100

    Finalizado

--- test_DyV.cpp
+++ test_DyV.cpp
@@ -2,7 +2,7 @@
 #include "DyV.h"
 
 
-void tryInt(int valor){
+void PruebaInt(int valor){
 	std::vector<int> a{0,1,2,3,4,5,6,7,8,9};
 	int pos = BusquedaBinaria(valor, a, 0, 10);
 	if(pos == -1){
@@ -13,8 +13,8 @@ void tryInt(int valor){
 	}
 }
 
-void tryFloat(float valor){
-	std::vector<float> a{0.1,1.1,2.1,3.1,4.1,5.1,6.1,7.1,8.1,9.1};
+void PruebaFloat(float valor){
+	std::vector<float> a{0.1,1.4,2.5,3.1,4.8,5.0,6.2,7.1,8.7,9.6};
 	int pos = BusquedaBinaria(valor, a, 0, 10);
 	if(pos == -1){
 		std::cout << "El valor " <<  valor << " no esta en la lista" << std::endl;
@@ -24,7 +24,7 @@ void tryFloat(float valor){
 	}
 }
 
-void tryChar(char valor){
+void PruebaChar(char valor){
 	std::vector<char> a{'A','B','C','D','E','a','b','c','d','e'};
 	int pos = BusquedaBinaria(valor, a, 0, 10);
 	if(pos == -1){
@@ -36,13 +36,19 @@ void tryChar(char valor){
 }
 	
 int main(){
-	int valorInt; std::cin >> valorInt;
-	float valorFloat; std::cin >> valorFloat;
-	char valorChar; std::cin >> valorChar;
+	int ValorInt; 
+	std::cout << "Introduce un valor entero para realizar la busqueda binaria: " << ' ';
+	std::cin >> ValorInt;
+	float ValorFloat; 
+	std::cout << "Introduce un valor flotante para realizar la busqueda binaria: "<<' ';
+	std::cin >> ValorFloat;
+	char ValorChar; 
+	std::cout << "Introduce un carácter para realizar la busqueda binaria: "<<' ';;
+	std::cin >> ValorChar;
 	
-	tryInt(valorInt);
-	tryFloat(valorFloat);
-	tryChar(valorChar);
+	PruebaInt(ValorInt);
+	PruebaFloat(ValorFloat);
+	PruebaChar(ValorChar);
 
 	return 0;
 }
